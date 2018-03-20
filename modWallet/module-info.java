ca7fcf3e1a6bbbc39eee747b3a5f4b4639328769
Otniel Nicola <otniel@centrys.io>
2018-03-21 00:02:32 +0200

    AW-50 - module builds correctly

--- modWallet/module-info.java
+++ modWallet/module-info.java
@@ -2,6 +2,10 @@ module aion.wallet{
 
     requires aion.apiserver;
     requires aion.zero.impl;
+    requires aion.base;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.graphics;
 
     exports org.aion.wallet;
     exports org.aion.wallet.ui;
