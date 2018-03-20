3b81b693e0d2cd37334542ead1fe12d9897feedf
Otniel Nicola <otniel@centrys.io>
2018-03-21 00:03:42 +0200

    AW-23 - Add image in header and shutdown button

--- modWallet/src/org/aion/wallet/ui/MainWindow.java
+++ modWallet/src/org/aion/wallet/ui/MainWindow.java
@@ -1,11 +1,13 @@
 package org.aion.wallet.ui;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.aion.api.server.ApiAion;
 import org.aion.base.type.Address;
 import org.aion.wallet.WalletApi;
@@ -14,34 +16,55 @@ import java.io.IOException;
 import java.util.List;
 
 public class MainWindow extends Application {
+    private double xOffset;
+    private double yOffset;
 
     private final ApiAion walletApi = new WalletApi();
 
     @Override
-    public void start(final Stage primaryStage) throws IOException {
+    public void start(final Stage stage) throws IOException {
+        stage.initStyle(StageStyle.TRANSPARENT);
 
         Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-        Scene scene = new Scene(root, 500, 350);
+        root.setOnMousePressed(this::handleMousePressed);
+        root.setOnMouseDragged(event -> handleMouseDragged(stage, event));
 
-        primaryStage.setOnCloseRequest(t -> ActionOnClose());
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        scene.setFill(Color.TRANSPARENT);
+
+        stage.setOnCloseRequest(t -> actionOnClose());
 
         final List<String> accounts = walletApi.getAccounts();
         if (!accounts.isEmpty()) {
-            final String account = accounts.get(0);
+            final Address address = Address.wrap(accounts.get(0));
             try {
-                primaryStage.setTitle(String.valueOf(walletApi.getBalance(Address.wrap(account))));
+                stage.setTitle(String.valueOf(walletApi.getBalance(address)));
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         } else {
-            primaryStage.setTitle("Aion Wallet Title");
+            stage.setTitle("Aion Wallet Title");
         }
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.setScene(scene);
+        stage.show();
     }
 
-    private void ActionOnClose() {
-        Platform.exit();
+    private void handleMousePressed(final MouseEvent event) {
+        xOffset = event.getSceneX();
+        yOffset = event.getSceneY();
+    }
+
+    private void handleMouseDragged(final Stage stage, final MouseEvent event) {
+        stage.setX(event.getScreenX() - xOffset);
+        stage.setY(event.getScreenY() - yOffset);
+    }
+
+    public void shutDown(){
+        actionOnClose();
+    }
+
+    private void actionOnClose() {
         System.exit(0);
     }
 }
