d382039ca394531abeb133a33970e5147f0a56c4
Otniel Nicola <otniel@centrys.io>
2018-03-21 00:23:18 +0200

    AW-23 - Add AION logo as taskbar Icon

--- modWallet/src/org/aion/wallet/ui/MainWindow.java
+++ modWallet/src/org/aion/wallet/ui/MainWindow.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -24,6 +25,7 @@ public class MainWindow extends Application {
     @Override
     public void start(final Stage stage) throws IOException {
         stage.initStyle(StageStyle.TRANSPARENT);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("aion_logo.png")));
 
         Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
         root.setOnMousePressed(this::handleMousePressed);
