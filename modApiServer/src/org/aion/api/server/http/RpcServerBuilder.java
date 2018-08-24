9da123c59984bbde7ca307ac4ceee581efa5b5a9
Paul Bochis <paul.bochis@gmail.com>
2018-08-24 14:02:35 +0300

    APB-93 add aion chain instance to abstract rpc server builder

--- modApiServer/src/org/aion/api/server/http/RpcServerBuilder.java
+++ modApiServer/src/org/aion/api/server/http/RpcServerBuilder.java
@@ -19,7 +19,7 @@ public abstract class RpcServerBuilder<T extends RpcServerBuilder<T>> {
     // but I personally like to avoid the visual clutter of accessors
     String hostName;
     Integer port;
-    IGenericAionChain aionChain;
+    protected IGenericAionChain aionChain;
 
     boolean corsEnabled = false;
     String corsOrigin = "*";
@@ -71,6 +71,11 @@ public abstract class RpcServerBuilder<T extends RpcServerBuilder<T>> {
     public T setWorkerPoolSize(Integer workerPoolSize) {
         this.workerPoolSize = workerPoolSize;
 
+        return self();
+    }
+
+    public T withAionChain(IGenericAionChain aionChain) {
+        this.aionChain = aionChain;
         return self();
     }
 
